25d68bf774789d8cf3354ec472e4d312bf4d1825
mukesh <mukesh@mukesh-Vostro-3520>
2024-06-04 16:55:38 +0545

    feat: Initialize App

---
+++ README.md
@@ -0,0 +1,7 @@
+## For Client Demo
+
+This app is created to customize requirement as per client for demo
+
+#### License
+
+MIT
---
+++ client_demo/config/desktop.py
@@ -0,0 +1,10 @@
+from frappe import _
+
+def get_data():
+	return [
+		{
+			"module_name": "For Client Demo",
+			"type": "module",
+			"label": _("For Client Demo")
+		}
+	]
